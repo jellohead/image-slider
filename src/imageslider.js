0b5881c580506a518fd65221a8039bbd4a31568f
pat <pat.escalona7@gmail.com>
2022-11-09 13:17:03 -0600

    working on regex to match image name

--- src/imageslider.js
+++ src/imageslider.js
@@ -4,8 +4,28 @@
 
 buttonHandler = (event) => {
     console.log('in buttonHandler');
+    const imgElement = document.querySelector('img');
+    console.log(imgElement);
+    console.log(document.querySelector('.image').src);
+    let imgSrc = document.querySelector('.image').src;
+
+
+
+
+    let imgNumber = imgSrc.search(/[0 - 9]/);
+    console.log(imgNumber);
+    imgNumber = imgSrc.search(/[0 - 9]\+/);
+    console.log(imgNumber);
+
     console.log(event);
     console.log(event.target);
+
+    console.log(event.target.outerText);
+
+    // const clickedButton = 
+    // let i = str.match
+    // event.target.outerText == '>' ? imgElement.src = `../resources/${i + 1}.jpeg` : console.log('left arrow');
+    // event.target.outerText == '>' ? console.log('right arrow' + imgElement) : console.log('left arrow');
 
 }
 
